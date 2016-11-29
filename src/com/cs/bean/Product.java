65c5a1c7b0faae0f79dc444cf46b5c187ae569d4
举 <861543705@qq.com>
2016-11-29 10:54:39 +0800

    这是我的主页

---
+++ src/com/cs/bean/Product.java
@@ -0,0 +1,53 @@
+package com.cs.bean;
+
+/**
+ * Created by 举 on 2016/11/29.
+ */
+public class Product {
+    private int id;
+    private String title;
+    private float price;
+    private String des;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public String getDes() {
+        return des;
+    }
+
+    public void setDes(String des) {
+        this.des = des;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", price=" + price +
+                ", des='" + des + '\'' +
+                '}';
+    }
+}
